0167a15a5f56a9b7904ddca9884775554dc80b96
Joey <mazzarelli@gmail.com>
2024-10-04 13:34:43 -0600

    checkpoint

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,68 @@
 // eslint-disable-next-line @typescript-eslint/no-require-imports
 const path = require('path');
 
+// module.exports = {
+//   entry: './src/CssVars.tsx',
+//   output: {
+//     filename: 'bundle.js',
+//     path: path.resolve(__dirname, 'dist'),
+//     publicPath: '/dist/',
+//   },
+//   resolve: {
+//     extensions: ['.ts', '.tsx', '.js'],
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.(ts|tsx)$/,
+//         use: 'ts-loader',
+//         exclude: /node_modules/,
+//       },
+//     ],
+//   },
+//   output: {
+//     filename: 'bundle.js',
+//     path: path.resolve(__dirname, 'dist'),
+//     publicPath: '/dist/',
+//   },
+//   resolve: {
+//     extensions: ['.ts', '.tsx', '.js'],
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.(ts|tsx)$/,
+//         use: 'ts-loader',
+//         exclude: /node_modules/,
+//       },
+//     ],
+//   },
+//   resolve: {
+//     extensions: ['.ts', '.tsx', '.js', '.d.ts'],
+//   },
+// };
+// const path = require('path');
+
 module.exports = {
   entry: './src/CssVars.tsx',
   output: {
-    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/dist/',
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/dist/',
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
+    library: 'CssVars',
+    libraryTarget: 'umd',
+    globalObject: 'this',
+    umdNamedDefine: true,
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.d.ts'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
   },
 };
