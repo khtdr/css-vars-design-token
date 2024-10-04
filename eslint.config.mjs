5b1355196c04501ca207f8f35b9b974bbd91f48d
Joey <mazzarelli@gmail.com>
2024-10-04 09:08:02 -0600

    more boilerplate

---
+++ eslint.config.mjs
@@ -0,0 +1,46 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { FlatCompat } from '@eslint/eslintrc';
+import react from 'eslint-plugin-react';
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import prettier from 'eslint-plugin-prettier';
+import js from '@eslint/js';
+import tsParser from '@typescript-eslint/parser';
+import globals from 'globals';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default [
+  ...compat.extends(
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ),
+  {
+    plugins: {
+      react,
+      '@typescript-eslint': typescriptEslint,
+      prettier,
+    },
+
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+
+      parser: tsParser,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+];
